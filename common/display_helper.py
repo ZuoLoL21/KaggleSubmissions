71ac94cfbf0ac8a85580769bfafb4b7ecaca5077
Zuolong Wu <zuolong2187@gmail.com>
2025-09-19 12:29:04 -0400

    More refactoring

--- common/display_helper.py
+++ common/display_helper.py
@@ -1,6 +1,8 @@
 from IPython.display import display_html
 from itertools import chain, cycle
 import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
 
 
 def display_side_by_side(*args, titles=cycle([''])):
@@ -18,3 +20,38 @@ def display_side_by_side(*args, titles=cycle([''])):
 
 def display_table(table):
 	display_html(table.to_html(), raw=True)
+
+
+def display_2d_hyperparameter_grid_search(hyperparameter_1: list,
+										  hyperparameter_2: list,
+										  results: dict,
+										  names: tuple[str, str] = ("x", "y"),
+										  ):
+	data = np.zeros((len(hyperparameter_1), len(hyperparameter_2)))
+
+	for i, hy1 in enumerate(hyperparameter_1):
+		for j, hy2 in enumerate(hyperparameter_2):
+			data[i, j] = results[(hy1, hy2)]
+
+	z_data = pd.DataFrame(data)
+
+	fig = go.Figure(data=[
+		go.Surface(x=list(hyperparameter_2), y=list(hyperparameter_1), z=z_data)
+	])
+	fig.update_traces(contours_z=dict(show=True,
+									  usecolormap=True,
+									  highlightcolor="limegreen",
+									  project_z=True))
+	fig.update_layout(title=dict(text='Hyperparameter tuning graph'),
+					  autosize=False,
+					  scene_camera_eye=dict(x=1.87, y=0.88, z=-0.64),
+					  width=500, height=500,
+					  margin=dict(l=65, r=50, b=65, t=90),
+					  scene=dict(
+							  xaxis_title=names[1],
+							  yaxis_title=names[0],
+							  zaxis_title="Accuracy"
+					  ),
+					  )
+
+	fig.show()

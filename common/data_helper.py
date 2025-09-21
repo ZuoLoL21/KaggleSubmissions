af3a8135c3022e8192b4a5ab74c5c029f4dde9e2
Zuolong Wu <zuolong2187@gmail.com>
2025-09-21 16:15:21 -0400

    Tried to make the stock market prediction work

--- common/data_helper.py
+++ common/data_helper.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 
 from common.display_helper import *
@@ -15,6 +16,17 @@ def summarize_categorical_columns_wrt_target(
 	)
 
 
+def summarize_continuous_columns_wrt_target(df: pd.DataFrame, variable_cols: str, col: str, row: str = None):
+	# Plot survival per Age bin
+	if row is None:
+		g = sns.FacetGrid(df, col=col, row=row)
+	else:
+		g = sns.FacetGrid(df, col=col, row=row, height=2, aspect=1.6)
+
+	g.map(plt.hist, variable_cols, bins=20)
+	plt.show()
+
+
 def describe_categorical_series(series: pd.Series):
 	display_side_by_side(
 			series.astype('category').describe(),
@@ -29,28 +41,23 @@ def describe_continuous_series(series: pd.Series):
 			titles=["Description", "Count"])
 
 
-def summarize_continuous_columns_wrt_target(df: pd.DataFrame, variable_cols: str, col: str, row: str = None):
-	# Plot survival per Age bin
-	if row is None:
-		g = sns.FacetGrid(df, col=col, row=row)
-	else:
-		g = sns.FacetGrid(df, col=col, row=row, height=2, aspect=1.6)
-
-	g.map(plt.hist, variable_cols, bins=20)
-	plt.show()
-
-
-def show_distribution(df: pd.DataFrame,
-					  variable_col: str,
-					  target_col: str,
-					  possible_vals: tuple = (0, 1),
-					  colors: tuple = ("green", "red"),
-					  max_x: int = None,
-					  min_x: int = 0,
-					  ):
+def show_distribution_wrt_target(df: pd.DataFrame,
+								 variable_col: str,
+								 target_col: str,
+								 possible_vals: tuple = (0, 1),
+								 colors: tuple = ("green", "red"),
+								 max_x: int = None,
+								 min_x: int = 0,
+								 ):
 	for val, color in zip(possible_vals, colors):
 		sns.kdeplot(df[variable_col][df[target_col] == val], color=color)
 
 	plt.legend([f"{target_col} = {x}" for x in possible_vals])
+	plt.xlim(min_x, max_x)
+	plt.show()
+
+
+def show_distribution(series: pd.Series, min_x: int = 0, max_x: int = None):
+	sns.kdeplot(series, color="green")
 	plt.xlim(min_x, max_x)
 	plt.show()
